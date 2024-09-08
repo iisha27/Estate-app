3fae4f37e2257f444e023cf31e56c794e2dba34b
Isha Khurana <ikhurana2709@gmail.com>
2024-09-08 15:27:06 +0530

    Fetch data from react-router-dom loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage";
+import { singlePageLoader } from "./lib/loaders";
 
 
 function App() {
@@ -29,7 +30,8 @@ function App() {
         },
         {
           path:"/:id",
-          element:<SinglePage/>
+          element:<SinglePage/>,
+          loader:singlePageLoader,
         },
         // {
         //   path:"/profile",
