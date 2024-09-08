b7642c6c908987c0ecbd2247c678e4016eb18566
Isha Khurana <ikhurana2709@gmail.com>
2024-09-08 14:33:23 +0530

    Created Post Modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+import NewPostPage from "./routes/newPostPage/newPostPage";
+
 
 function App() {
   const router = createBrowserRouter([
@@ -54,6 +56,10 @@ function App() {
         {
           path:"/profile/update",
           element:<ProfileUpdatePage />
+        },
+        {
+          path:"/add",
+          element:<NewPostPage />
         },
       ]
     }
